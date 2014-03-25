741f9dff2463c83f3d979ea3f7aaf23736e42832
crossjs <one@ffully.com>
2014-03-25 21:35:21 +0800

    update api doc

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Aspect",
+        "Widget"
+    ],
+    "modules": [
+        "Widget"
+    ],
+    "allModules": [
+        {
+            "displayName": "Widget",
+            "name": "Widget",
+            "description": "组件基类"
+        }
+    ]
+} };
+});
